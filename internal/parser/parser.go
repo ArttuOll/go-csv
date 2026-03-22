d333f9ebb7530147f929f7e0f9651528df520f7a
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2026-03-22 11:40:52 +0200

    WIP: Move to recursive descent

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -2,20 +2,34 @@ package parser
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
-	"strings"
+)
+
+type ParserState int
+
+const (
+	ParsingField = iota
+	ParsingRecord
 )
 
 type CsvParser struct {
-	scanner         *bufio.Scanner
+	reader          io.Reader
 	fieldsInARecord int
 	currentLine     int
+	line            string
+	state           ParserState
+	buffer          []byte
+	readToIndex     int
 }
+
+const BUFFER_SIZE = 8
 
 func NewCsvParser(r io.Reader) *CsvParser {
 	return &CsvParser{
-		scanner: bufio.NewScanner(r),
+		reader: bufio.NewReader(r),
+		buffer: make([]byte, BUFFER_SIZE),
 	}
 }
 
@@ -29,76 +43,150 @@ func (error *CsvParseError) Error() string {
 }
 
 func (parser *CsvParser) ParseAll() (records [][]string, err error) {
-
-	for parser.scanner.Scan() {
+	for {
 		nextLine, err := parser.parseLine()
 		if err != nil {
-			return records, err
+			if errors.Is(err, io.EOF) {
+				return records, nil
+			}
+
+			return nil, err
 		}
 
 		records = append(records, nextLine)
 	}
-
-	return records, nil
 }
 
 func (parser *CsvParser) Parse() (record []string, err error) {
-	parser.scanner.Scan()
-
 	return parser.parseLine()
 }
 
 func (parser *CsvParser) parseLine() (record []string, err error) {
-	line := parser.scanner.Text()
+	for {
+		if parser.readToIndex >= len(parser.buffer) {
+			newBuffer := make([]byte, len(parser.buffer)*2)
+			copy(newBuffer, parser.buffer)
+			parser.buffer = newBuffer
+		}
 
-	fields, err := parser.parseRecord(line)
-	if err != nil {
-		return nil, err
-	}
-
-	parser.currentLine++
-
-	if parser.fieldsInARecord == 0 {
-		parser.fieldsInARecord = len(fields)
-	}
-
-	numberOfFields := len(fields)
-	if numberOfFields != parser.fieldsInARecord {
-		return nil, &CsvParseError{Line: parser.currentLine, Message: fmt.Sprintf("failed to parse line. too many fields in a record: %v, but should be %v", numberOfFields, parser.fieldsInARecord)}
-	}
-
-	return fields, nil
-}
-
-func (parser *CsvParser) parseRecord(line string) ([]string, error) {
-	withoutLineBreak, _ := strings.CutSuffix(line, "\r\n")
-
-	if strings.HasSuffix(withoutLineBreak, ",") {
-		return nil, &CsvParseError{Line: parser.currentLine, Message: "the last field in a record can't be followed by a comma."}
-	}
-
-	var fields []string
-	for rawField := range strings.SplitSeq(withoutLineBreak, ",") {
-		validField, err := parser.parseField(rawField)
+		// Attempt to parse data received so far
+		record, charsParsed, err := parser.parseRecord(string(parser.buffer[:parser.readToIndex]))
 		if err != nil {
 			return nil, err
 		}
 
-		fields = append(fields, validField)
-	}
+		// Not enough data in the buffer. Read more.
+		if charsParsed == 0 {
+			numberOfBytesInChunk, err := parser.reader.Read(parser.buffer[parser.readToIndex:])
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					return record, err
+				}
 
-	return fields, nil
+				return nil, err
+			}
+
+			parser.readToIndex += numberOfBytesInChunk
+			continue
+		}
+
+		parser.currentLine++
+
+		// Succeeded in parsing record. Remove the parsed record from the buffer.
+		copy(parser.buffer, parser.buffer[charsParsed:])
+		parser.readToIndex -= charsParsed
+		return record, nil
+	}
 }
 
-func (parser *CsvParser) parseField(input string) (string, error) {
-	withoutOpeningDoubleQuote, foundOpening := strings.CutPrefix(input, `"`)
-	withoutSurroundingDoubleQuotes, foundClosing := strings.CutSuffix(withoutOpeningDoubleQuote, `"`)
+func (parser *CsvParser) parseRecord(input string) (fields []string, totalCharsRead int, err error) {
+	position := 0
+	for {
+		field, charsRead, err := parser.parseField(input[position:])
+		if err != nil {
+			return nil, 0, err
+		}
 
-	if foundOpening && foundClosing {
-		if strings.Contains(withoutSurroundingDoubleQuotes, `"`) {
-			return "", &CsvParseError{Line: parser.currentLine, Message: fmt.Sprintf("fields enclosed in double quotes may not contain double quotes: %v", input)}
+		// Need more data
+		if charsRead == 0 {
+			return nil, 0, nil
+		}
+
+		fields = append(fields, field)
+		position += len(field)
+		totalCharsRead += charsRead
+
+		if input[position] == ',' {
+			position++
+			totalCharsRead++
+		}
+
+		if input[position] == '\r' {
+			position++
+			totalCharsRead++
+		}
+
+		if input[position] == '\n' {
+			totalCharsRead++
+			numberOfFields := len(fields)
+
+			if parser.fieldsInARecord == 0 {
+				parser.fieldsInARecord = len(fields)
+			} else if numberOfFields != parser.fieldsInARecord {
+				return nil, 0, &CsvParseError{Line: parser.currentLine, Message: fmt.Sprintf("failed to parse record. too many fields in a record: %v, but should be %v", numberOfFields, parser.fieldsInARecord)}
+			}
+
+			return fields, totalCharsRead, nil
+		}
+
+	}
+}
+
+func (parser *CsvParser) parseField(input string) (string, int, error) {
+	escaped := false
+	escapingDoubleQuote := false
+
+	for i, v := range input {
+		if i == 0 && v == '"' {
+			escaped = true
+			continue
+		}
+
+		// Final field ends
+		if !escaped && (v == '\r' || v == '\n') {
+			return input[:i], i, nil
+		}
+
+		if !escaped && v == '"' {
+			return "", 0, &CsvParseError{Line: parser.currentLine, Message: "fields containing double quotes must be enclosed by double quotes. the contained double quote must be escaped with a preceding double quote."}
+		}
+
+		// Field ends
+		if !escaped && v == ',' {
+			return input[:i], i, nil
+		}
+
+		if v == '"' {
+			// Closing double quote, field ends
+			if i+1 == len(input) {
+				return input[1 : len(input)-1], i + 1, nil
+			}
+
+			// Escaped double quote sequence
+			if input[i+1] == '"' {
+				escapingDoubleQuote = true
+				continue
+			}
+
+			if escapingDoubleQuote {
+				escapingDoubleQuote = false
+				continue
+			}
+
+			return "", 0, &CsvParseError{Line: parser.currentLine, Message: "double quotes within double quote enclosed fields must be escaped with a preceding double quote"}
 		}
 	}
 
-	return input, nil
+	// The input doesn't contain a complete field. We need more data.
+	return "", 0, nil
 }
